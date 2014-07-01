c6646d9d7441a741c838f34ba3df0c79351a6c9b
Xuanzhi Zhang <zxzsaga@gmail.com>
2014-07-01 21:22:15 +0800

    get simple bookmark tree

--- js/newtab.js
+++ js/newtab.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
-    chrome.bookmarks.getTree(function(arr, result) {
-        console.log(JSON.stringify(arr, null, '  '));
+    chrome.bookmarks.getTree(function(bookmarkTree, result) {
+        var simpleBookmarkTree = {};
+        bookmarkTree.forEach(function(oneOfSon) {
+            addSon(simpleBookmarkTree, oneOfSon);
+        });
+        console.log(simpleBookmarkTree);
     });
 });
+
+function addSon(father, son) {
+    if (son.hasOwnProperty('children')) {
+        father[son.title] = {};
+        son.children.forEach(function(oneOfSon) {
+            addSon(father[son.title], oneOfSon);
+        });
+    }
+    else {
+        father[son.title] = son.url;
+    }
+}
+
+function appendSubMenu() {
+
+}
