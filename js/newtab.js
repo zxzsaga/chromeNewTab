7b5babced11c1998d8c3a61d8b6dfaf66928897e
Xuanzhi Zhang <zxzsaga@gmail.com>
2014-07-08 16:03:55 +0800

    add lv1 folder

--- js/newtab.js
+++ js/newtab.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
             addSon(simpleBookmarkTree, oneOfSon);
         });
         console.log(simpleBookmarkTree);
+
+        appendSubMenu(simpleBookmarkTree[''], true)
     });
 });
 
@@ -20,6 +22,77 @@ function addSon(father, son) {
     }
 }
 
-function appendSubMenu() {
+function appendSubMenu(father, isLv1, fatherName) {
+    for (var son in father) {
+        if (isLv1) { // lv1 always folder
+            var a = $('<a></a>')
+                .attr('data-toggle', 'collapse')
+                .attr('data-parent', '#accordion')
+                .attr('href', '#'+ son)
+                .text(son);
+            var panelTitle = $('<h4></h4>')
+                .addClass('panel-title')
+                .append(a);
+            var panelHeading = $('<div></div>')
+                .addClass('panel-heading')
+                .append(panelTitle);
 
+            var panelBody = $('<div></div>')
+                .addClass('panel-body');
+            var zzzzzzz = appendSubMenu(father[son], false, son);
+            panelBody.append(zzzzzzz);
+            var panelBodyFather = $('<div></div>')
+                .attr('id', son)
+                .addClass('panel-collapse')
+                .addClass('collapse')
+                .addClass('in')
+
+            var panel = $('<div></div>')
+                .addClass('panel')
+                .addClass('panel-default')
+                .append(panelHeading)
+                .append(panelBodyFather);
+
+            $('#accordion').append(panel);
+        }
+        else {
+            if (typeof father[son] === 'string') {
+                var a = $('<a></a>')
+                    .text(son)
+                    .attr('href', father[son]);
+                return a;
+                // $('#' + sonName + '.panel-body').append(a);
+            }
+            else if (typeof father[son] === 'object') {
+                var a = $('<a></a>')
+                    .attr('data-toggle', 'collapse')
+                    .attr('data-parent', '#' + fatherName)
+                    .attr('href', '#'+ son)
+                    .text(son);
+                var panelTitle = $('<h4></h4>')
+                    .addClass('panel-title')
+                    .append(a);
+                var panelHeading = $('<div></div>')
+                    .addClass('panel-heading')
+                    .append(panelTitle);
+
+                var panelBody = $('<div></div>')
+                    .addClass('panel-body');
+                var zzzzzzz = appendSubMenu(father[son], false, son);
+                panelBody.append(zzzzzzz);
+                var panelBodyFather = $('<div></div>')
+                    .attr('id', son)
+                    .addClass('panel-collapse')
+                    .addClass('collapse')
+                    .addClass('in')
+
+                var panel = $('<div></div>')
+                    .addClass('panel')
+                    .addClass('panel-default')
+                    .append(panelHeading)
+                    .append(panelBodyFather);
+                return panel;
+            }
+        }
+    }
 }
